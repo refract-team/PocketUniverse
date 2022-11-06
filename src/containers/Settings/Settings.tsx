782cc65d6fbb1f4bac5fd4c11ca45274eff1fc5a
Justin Phu <me@justinphu.dev>
2022-11-06 13:49:54 +1100

    feat: add blur to one of the contracts that will be skipped

--- src/containers/Settings/Settings.tsx
+++ src/containers/Settings/Settings.tsx
@@ -133,7 +133,7 @@ const Settings = ({ settingsOpen }: { settingsOpen: boolean }) => {
             <div className="flex flex-col text-lg text-gray-100 my-auto">
               Sniper Mode
               <span className="text-gray-100 text-xs">
-                Skips the popup for purchases on the OS/LR/X2 contracts - use at your own risk!
+                Skips the popup for purchases on the OS/LR/X2/Blur contracts - use at your own risk!
               </span>
             </div>
             {address && premium ?
